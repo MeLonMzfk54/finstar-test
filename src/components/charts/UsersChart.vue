2a2aa82717abd6623bac42520f202e95868646f0
Anton Garifullin <garifullin.antoshka@mail.ru>
2022-06-28 02:31:02 +0700

    stable version without charts

--- src/components/charts/UsersChart.vue
+++ src/components/charts/UsersChart.vue
@@ -1,62 +1,14 @@
 <template>
   <div class="users-chart">
-    <Bar
-        :chart-options="chartOptions"
-        :chart-data="chartData"
-        :chart-id="chartId"
-        :dataset-id-key="datasetIdKey"
-        :plugins="plugins"
-        :css-classes="cssClasses"
-        :styles="styles"
-        :width="width"
-        :height="height"
-    />
+
   </div>
 </template>
 
 <script>
-  import { Bar } from 'vue-chartjs'
-  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-
-  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
   export default {
     name: 'usersChart',
-    components: {Bar},
-    props: {
-      chartId: {
-        type: String,
-        default: 'bar-chart'
-      },
-      datasetIdKey: {
-        type: String,
-        default: 'label'
-      },
-      width: {
-        type: Number,
-        default: 400
-      },
-      height: {
-        type: Number,
-        default: 400
-      },
-      cssClasses: {
-        default: '',
-        type: String
-      },
-      styles: {
-        type: Object,
-        default: () => {}
-      },
-      plugins: {
-        type: Object,
-        default: () => {}
-      }
-    },
     data(){
       return {
-        chartOptions: {
-          responsive: true
-        }
       }
     },
     computed: {
