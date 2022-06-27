85fdec1991e9cf6d46791cbc73558b36347f95db
Anton Garifullin <garifullin.antoshka@mail.ru>
2022-06-27 21:13:44 +0700

    fetch data + store

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,32 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    lists: [],
   },
   getters: {
+    groupedLists: state => {
+      const map = state.lists.reduce((prev, current) => {
+        prev[current.userId] = prev[current.userId] || [];
+        prev[current.userId].push(current);
+        return prev;
+      }, {});
+      const result = [];
+      for (let key in map) {
+        result.push(map[key]);
+      }
+      return result;
+    }
   },
   mutations: {
+    'SET_LISTS'(state, lists) {
+      state.lists = lists;
+    }
   },
   actions: {
+    async fetchLists({commit}) {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos').then(resp => resp.json());
+      commit('SET_LISTS', response);
+    }
   },
   modules: {
   }
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,5 +1,45 @@
 <template>
-  <div>
-    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad at beatae consequatur debitis dicta, dolorum hic maiores molestiae nam nemo odio optio provident quasi quia totam unde voluptate, voluptatibus. Aliquid dolorem, eum numquam porro quod sapiente tempora voluptates. Amet, doloremque impedit magnam non praesentium quia quod reiciendis rem sunt.</h1>
+  <div class="main">
+    <div class="container">
+      <button class="main__button" @click="getLists">
+        Загрузить данные
+      </button>
+      <div>
+        {{ lists }}
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "MainView",
+  data() {
+    return {}
+  },
+  computed: {
+    lists() {
+      return this.$store.getters.groupedLists;
+    }
+  },
+  methods: {
+    async getLists() {
+      await this.$store.dispatch('fetchLists')
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+  .main {
+    padding: 10px 0px;
+    &__button {
+      padding: 10px;
+      transition: .4s all ease;
+      &:hover {
+        transform: scale(1.05);
+      }
+    }
+  }
+</style>
